067dd0761202c21c3d77a29432c0a0d26445f151
Lorae Stojanovic <stojanovic.lorae@gmail.com>
2025-10-10 13:45:46 -0400

    chore: create run-all script

---
+++ run-all.R
@@ -0,0 +1,17 @@
+# Source this file to replicate the entire project.
+# BEFORE RUNNING: Create the .Renviron per the instructions in README.md.
+
+# R environment setup
+renv::restore()
+
+# Read and clean data
+source("src/import-ipums-usa.R")
+source("src/process-ipums-usa-person.R")
+source("src/process-ipums-usa-household.R")
+
+# Create figures and results
+source("src/figures/fig01-hhsize-decades-line.R")
+source("src/figures/fig02-household-members.R")
+source("src/figures/fig03-multifam-decades-line.R")
+source("src/figures/fig04-hoh-housemates-normalized-line.R")
+
