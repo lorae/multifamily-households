9830b582bad8a4e7f34310997fe9419e3d94bdae
Lorae <10428186+lorae@users.noreply.github.com>
2025-10-14 09:38:25 -0400

    chore: update run-all

--- run-all.R
+++ run-all.R
@@ -15,3 +15,8 @@ source("src/figures/fig02-household-members.R")
 source("src/figures/fig03-multifam-decades-line.R")
 source("src/figures/fig04-hoh-housemates-normalized-line.R")
 
+# k-means ML
+source("src/cluster-elbow-plot.R") # choose k from k=1 to k=15 (outputs fig05)
+source("src/cluster-k6-selection.R") # choose among k = 6 model outputs
+
+
